5a6c11d7b454a1effb96e3a15997e7a4b88430d9
kanzul <muhammad.mf585@gmail.com>
2024-06-26 20:40:48 +0700

    home screen layout

--- src/screens/MovieDetail.tsx
+++ src/screens/MovieDetail.tsx
@@ -1,13 +1,29 @@
-import React from 'react'
-import { View, Text, Button } from 'react-native'
-import { API_URL, API_ACCESS_TOKEN } from '@env' // Ditambahkan
+import React, { useEffect, useState } from 'react'
+import {
+    Text,
+    View,
+    StyleSheet,
+    ImageBackground,
+    ScrollView,
+} from 'react-native'
 
-const MovieDetail = ({ navigation }: any): any => {
-    const fetchData = (): void => {
-        if (API_URL == null || API_ACCESS_TOKEN.length == null) {
-            throw new Error('ENV not found')
-        }
+import { API_ACCESS_TOKEN } from '@env'
+import { FontAwesome } from '@expo/vector-icons'
+import { LinearGradient } from 'expo-linear-gradient'
+// import { LinearGradient } from 'react-native-linear-gradient'
+import type { Movie } from '../types/app'
+import MovieList from '../components/movies/MovieList'
 
+const MovieDetail = ({ route }: any): JSX.Element => {
+    const { id } = route.params
+    const [movie, setMovie] = useState<Movie | null>(null)
+
+    useEffect(() => {
+        getMovieDetail()
+    }, [])
+
+    const getMovieDetail = (): void => {
+        const url = `https://api.themoviedb.org/3/movie/${id}`
         const options = {
             method: 'GET',
             headers: {
@@ -16,27 +32,146 @@ const MovieDetail = ({ navigation }: any): any => {
             },
         }
 
-        fetch(API_URL, options)
-            .then((response) => response.json())
+        fetch(url, options)
+            .then(async (response) => await response.json())
             .then((response) => {
-                console.log(response)
+                setMovie(response)
             })
-            .catch((err) => {
-                console.error(err)
+            .catch((errorResponse) => {
+                console.log(errorResponse)
             })
     }
 
+    if (!movie) {
+        return (
+            <View style={styles.loadingContainer}>
+                <Text>Loading...</Text>
+            </View>
+        )
+    }
+
+    const renderStars = (rating: number) => {
+        const stars = []
+        for (let i = 1; i <= 5; i++) {
+            const name = i <= rating ? 'star' : 'star-o'
+            stars.push(<FontAwesome key={i} name={name} size={24} color="yellow" />)
+        }
+        return stars
+    }
+
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Movie Detail Page</Text>
-            <Button
-                title="Fetch Data"
-                onPress={() => {
-                    fetchData()
+        <ScrollView>
+            <ImageBackground
+                source={{
+                    uri: `https://image.tmdb.org/t/p/w500${movie.backdrop_path}`,
                 }}
-            />
-        </View>
+                style={styles.backgroundImage}
+            >
+                <LinearGradient
+                    colors={['transparent', 'rgba(0,0,0,0.8)']}
+                    style={styles.gradient}
+                >
+                    <Text style={styles.title}>{movie.title}</Text>
+                    <View style={styles.ratingContainer}>
+                        {renderStars(Math.round(movie.vote_average / 2))}
+                        <Text style={styles.ratingText}>{movie.vote_average}</Text>
+                    </View>
+                </LinearGradient>
+            </ImageBackground>
+            <View style={styles.container}>
+                <Text style={styles.overview}>{movie.overview}</Text>
+                <View style={styles.detailRow}>
+                    <View style={styles.detailColumn}>
+                        <Text style={styles.detailLabel}>Original Language</Text>
+                        <Text style={styles.detailValue}>{movie.original_language}</Text>
+                    </View>
+                    <View style={styles.detailColumn}>
+                        <Text style={styles.detailLabel}>Popularity</Text>
+                        <Text style={styles.detailValue}>{movie.popularity}</Text>
+                    </View>
+                </View>
+
+                <View style={styles.detailRow}>
+                    <View style={styles.detailColumn}>
+                        <Text style={styles.detailLabel}>Release Date</Text>
+                        <Text style={styles.detailValue}>
+                            {new Date(movie.release_date).toDateString()}
+                        </Text>
+                    </View>
+                    <View style={styles.detailColumn}>
+                        <Text style={styles.detailLabel}>Vote Count</Text>
+                        <Text style={styles.detailValue}>{movie.vote_count}</Text>
+                    </View>
+                </View>
+                <MovieList
+                    title="Recommendations"
+                    path={`movie/${id}/recommendations?language=en-US&page=1`}
+                    coverType="poster"
+                />
+            </View>
+        </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    backgroundImage: {
+        width: '100%',
+        height: 300,
+        justifyContent: 'flex-end',
+    },
+    gradient: {
+        width: '100%',
+        padding: 16,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: 'white',
+        marginBottom: 8,
+    },
+    ratingContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    ratingText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'yellow',
+        marginLeft: 8,
+    },
+    container: {
+        padding: 16,
+    },
+    loadingContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    overview: {
+        fontSize: 16,
+        marginBottom: 16,
+    },
+    detailContainer: {
+        marginBottom: 16,
+    },
+    detailRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingVertical: 10,
+    },
+    detailColumn: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'flex-start',
+    },
+    detailLabel: {
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    detailValue: {
+        fontSize: 16,
+        color: 'black',
+    },
+})
 
 export default MovieDetail
